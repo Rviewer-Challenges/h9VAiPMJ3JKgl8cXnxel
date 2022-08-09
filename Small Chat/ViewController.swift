48162f3b722f6b1a17790fe3a70331c50bcba1e2
Juan <juanjoseen@gmail.com>
2022-08-08 21:25:54 -0500

    First commit

---
+++ Small Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Small Chat
+//
+//  Created by Juan Jose Elias Navarro on 08/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
